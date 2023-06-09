9efdf26e5612aee47f256355c12eb14b9db29911
rae <raej0000@gmail.com>
2023-06-09 08:19:57 -0700

    (hopefully) fixed molten bronze texture + fixed molten bronze properties (mimic lava)

--- src/main/java/net/rae/bronze_age/fluid/ModFluidTypes.java
+++ src/main/java/net/rae/bronze_age/fluid/ModFluidTypes.java
@@ -3,7 +3,9 @@ package net.rae.bronze_age.fluid;
 import com.mojang.math.Vector3f;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.level.pathfinder.BlockPathTypes;
 import net.minecraftforge.common.SoundAction;
+import net.minecraftforge.common.SoundActions;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fluids.FluidType;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,18 +14,24 @@ import net.minecraftforge.registries.RegistryObject;
 import net.rae.bronze_age.BronzeAge;
 
 public class ModFluidTypes {
-    public static final ResourceLocation WATER_STILL_RL = new ResourceLocation("block/mbronze_still");
-    public static final ResourceLocation WATER_FLOWING_RL = new ResourceLocation("block/mbronze_flow");
-    public static final ResourceLocation SOAP_OVERLAY_RL = new ResourceLocation(BronzeAge.MOD_ID, "misc/in_molten_bronze");
+    public static final ResourceLocation WATER_STILL_RL = new ResourceLocation(BronzeAge.MOD_ID, "block/mbronze_still");
+    public static final ResourceLocation WATER_FLOWING_RL = new ResourceLocation(BronzeAge.MOD_ID, "block/mbronze_flow");
+    public static final ResourceLocation BRONZE_OVERLAY_RL = new ResourceLocation(BronzeAge.MOD_ID, "misc/in_molten_bronze");
 
     public static final DeferredRegister<FluidType> FLUID_TYPES =
             DeferredRegister.create(ForgeRegistries.Keys.FLUID_TYPES, BronzeAge.MOD_ID);
 
     public static final RegistryObject<FluidType> MOLTEN_BRONZE_FLUID_TYPE = register("molten_bronze_fluid",
-            FluidType.Properties.create().lightLevel(7).density(15).viscosity(5).sound(SoundAction.get("drink"), SoundEvents.PLAYER_HURT));
+            FluidType.Properties.create().lightLevel(15).density(3000).viscosity(6000)
+                    .sound(SoundAction.get("drink"), SoundEvents.PLAYER_HURT)
+                    .sound(SoundActions.BUCKET_FILL, SoundEvents.BUCKET_FILL_LAVA)
+                    .sound(SoundActions.BUCKET_EMPTY, SoundEvents.BUCKET_EMPTY_LAVA)
+                    .temperature(1100).pathType(BlockPathTypes.LAVA).adjacentPathType(null)
+                    .canSwim(false).canDrown(false)
+    );
 
     private static RegistryObject<FluidType> register(String name, FluidType.Properties properties) {
-        return FLUID_TYPES.register(name, () -> new BaseFluidType(WATER_STILL_RL, WATER_FLOWING_RL, SOAP_OVERLAY_RL,
+        return FLUID_TYPES.register(name, () -> new BaseFluidType(WATER_STILL_RL, WATER_FLOWING_RL, BRONZE_OVERLAY_RL,
                 0xA1975e25, new Vector3f(151f / 255f, 94f / 255f, 37f / 255f), properties));
     }
 
